e00d71d0284e7c2ddbb7bb3ae9888e8a142d3310
Greg Sweet <greg@ccdzine.com>
2018-06-09 10:38:19 -0800

    Begin sample editor (Gutenberg) block
    New registry class file. So far it does nothing.

---
+++ includes/editor-blocks/class-register-block-types.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Register custom editor blocks.
+ *
+ * @link       http://ccdzine.com
+ * @since      1.0.0
+ *
+ * @package    controlled-chaos
+ * @subpackage controlled-chaos/includes/editor-blocks
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Register custom editor blocks.
+ *
+ * @since      1.0.0
+ * @package    controlled-chaos
+ * @subpackage controlled-chaos/includes/editor-blocks
+ * @author     Greg Sweet <greg@ccdzine.com>
+ */
+class Controlled_Chaos_Register_Blocks {
+
+	/**
+	 * Initialize the class.
+	 *
+	 * @since    1.0.0
+	 */
+	public function __construct() {
+
+
+		
+	}
+
+}
+
+$ccp_register_blocks = new Controlled_Chaos_Register_Blocks();
