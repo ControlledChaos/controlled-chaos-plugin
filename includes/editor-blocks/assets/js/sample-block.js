926c0d674d5f272c3df5832450025efb1e5924d3
Greg Sweet <greg@ccdzine.com>
2018-06-09 17:09:16 -0800

    Basic sample editor block
    Added a very basic editor (Gutenberg) block as a sample/jump start. This
    may be removed as an editable block evolves here. Just getting the new
    editor ball rolling.

---
+++ includes/editor-blocks/assets/js/sample-block.js
@@ -0,0 +1,65 @@
+/**
+ * Very simple sample block.
+ *
+ * Registering a basic block with Gutenberg.
+ * Simple block, renders and saves the same content without any interactivity.
+ * 
+ * This may be removed as an editable sample block evolves.
+ * 
+ * @since 1.0.0
+ */
+
+( function() {
+	// The __() for internationalization.
+	var __ = wp.i18n.__;
+
+	// The wp.element.createElement() function to create elements.
+	var el = wp.element.createElement;
+
+	// The registerBlockType() to register blocks.
+	var registerBlockType = wp.blocks.registerBlockType;
+
+	/**
+	* Register Basic Block.
+	*
+	* Registers a new block provided a unique name and an object defining its
+	* behavior. Once registered, the block is made available as an option to any
+	* editor interface where blocks are implemented.
+	*
+	* @param  {string}   name     Block name.
+	* @param  {Object}   settings Block settings.
+	* @return {?WPBlock}          The block, if it has been successfully
+	*                             registered; otherwise `undefined`.
+	*/
+
+	/**
+	* Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
+	* Block icon from Dashicons.
+	* Block category common, formatting, layout, widgets, embed.
+	*/
+	registerBlockType( 'ccp/sample-block', {
+		title    : __( 'Sample Block', 'controlled-chaos' ),
+		icon     : 'lightbulb',
+		category : 'common',
+
+		// The "edit" property must be a valid function.
+		edit: function(props) {
+			// Creates a <p class='wp-block-ccp-sample-block"'></p>.
+			return el(
+				'p', // Tag type.
+				{ className: props.className }, // The class="wp-block-ccp-sample-block" : The class name is generated using the block's name prefixed with wp-block-, replacing the / namespace separator with a single -.
+				'Sample Block' // Content inside the tag.
+			);
+		},
+
+		// The "save" property must be specified and must be a valid function.
+		save: function(props) {
+			return el(
+				'p', // Tag type.
+				{ className: props.className }, // The class="wp-block-ccp-sample-block" : The class name is generated using the block's name prefixed with wp-block-, replacing the / namespace separator with a single -.
+				'Sample Block' // Content inside the tag.
+			);
+		}
+	});
+})
+();
--- includes/editor-blocks/class-register-block-types.php
+++ includes/editor-blocks/class-register-block-types.php
@@ -32,8 +32,59 @@ class Controlled_Chaos_Register_Blocks {
 	 */
 	public function __construct() {
 
+		// Enqueue sample block backend assets.
+		add_action( 'enqueue_block_editor_assets', [ $this, 'sample_block_editor_assets' ] );
 
-		
+		// Enqueue sample block frontend assets.
+		add_action( 'enqueue_block_assets', [ $this, 'sample_block_frontend_assets' ] );
+
+	}
+
+	/**
+	 * Enqueue sample block backend assets.
+	 * 
+	 * `wp-blocks`: includes block type registration and related functions.
+	 * `wp-element`: includes the WordPress Element abstraction for describing the structure of your blocks.
+	 * `wp-i18n`: To internationalize the block's text.
+	 *
+	 * @since    1.0.0
+	 * @access   public
+	 */
+	public function sample_block_editor_assets() {
+
+		// Sample block scripts.
+		wp_enqueue_script(
+			'ccp-sample-block-script', // Handle.
+			plugins_url( 'assets/js/sample-block.min.js', __FILE__ ), // Block.js: We register the block here.
+			[ 'wp-blocks', 'wp-i18n', 'wp-element' ], // Dependencies, defined above.
+			filemtime( plugin_dir_path( __FILE__ ) . 'assets/js/sample-block.min.js' ) // filemtime — Gets file modification time.
+		);
+
+		// Sample block styles.
+		wp_enqueue_style(
+			'ccp-sample-block', // Handle.
+			plugins_url( 'assets/css/sample-block.min.css', __FILE__ ), // Block editor CSS.
+			[ 'wp-edit-blocks' ], // Dependency to include the CSS after it.
+			filemtime( plugin_dir_path( __FILE__ ) . 'assets/css/sample-block.min.css' ) // filemtime — Gets file modification time.
+		);
+
+	}
+
+	/**
+	 * Enqueue sample block frontend assets.
+	 *
+	 * @since    1.0.0
+	 * @access   public
+	 */
+	public function sample_block_frontend_assets() {
+
+		wp_enqueue_style(
+			'ccp-sample-block',
+			plugins_url( 'assets/css/sample-block.css', __FILE__ ),
+			[ 'wp-blocks' ],
+			filemtime( plugin_dir_path( __FILE__ ) . 'assets/css/sample-block.css' )
+		);
+
 	}
 
 }
