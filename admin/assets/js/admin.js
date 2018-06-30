54a6ba8a2b086311aaa9bc5317e3716f00823930
Greg Sweet <greg@ccdzine.com>
2018-06-30 11:06:22 -0800

    Minor comment and arrangement tweaks

--- admin/assets/js/admin.js
+++ admin/assets/js/admin.js
@@ -1,6 +1,17 @@
 /**
- * Tabify the #tabs wrapper.
+ * Admin scripts.
+ * 
+ * This file is enqueued only on the backend of your site.
+ * Remember to minify this and change the file reference in
+ * admin > admin.php.
  */
-jQuery(document).ready(function ($) {
+
+/**
+ * Tabify the .ccp_tabbed-content wrapper.
+ * 
+ * @since  1.0.0
+ * @access public
+ */
+jQuery(document).ready( function ($) {
 	$( '.ccp_tabbed-content' ).tabs();
 });
