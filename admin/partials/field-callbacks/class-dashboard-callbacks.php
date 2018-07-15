998283e4af26b4eb08e0eb74fbe7e0f4278ec001
Greg Sweet <greg@ccdzine.com>
2018-07-15 08:35:19 -0800

    Started putting settings field callbacks intp partials

---
+++ admin/partials/field-callbacks/class-dashboard-callbacks.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Callbacks for the Dashboard tab on the Site Settings page.
+ *
+ * @package    Controlled_Chaos_Plugin
+ * @subpackage Admin\Partials\Field_Callbacks
+ *
+ * @since      1.0.0
+ * @author     Greg Sweet <greg@ccdzine.com>
+ */
+
+namespace CC_Plugin\Admin\Partials\Field_Callbacks;
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Callbacks for the Dashboard tab.
+ *
+ * @since  1.0.0
+ * @access public
+ */
+class Dashboard_Callbacks {
+
+	/**
+	 * Get an instance of the plugin class.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return object Returns the instance.
+	 */
+	public static function instance() {
+
+		// Varialbe for the instance to be used outside the class.
+		static $instance = null;
+
+		if ( is_null( $instance ) ) {
+
+			// Set variable for new instance.
+			$instance = new self;
+
+		}
+
+		// Return the instance.
+		return $instance;
+
+	}
+
+	/**
+	 * Constructor method.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return self
+	 */
+	public function __construct() {
+
+		add_action( 'ccp_custom_welcome', [ $this, 'custom_welcome' ] );
+
+	}
+
+	/**
+	 * Use custom welcome panel.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  array $args Extra arguments passed into the callback function.
+	 * @return string
+	 */
+	public function custom_welcome( $args ) {
+
+		$option = get_option( 'ccp_custom_welcome' );
+
+		$html = '<p><input type="checkbox" id="ccp_custom_welcome" name="ccp_custom_welcome" value="1" ' . checked( 1, $option, false ) . '/>';
+
+		$html .= '<label for="ccp_custom_welcome"> ' . $args[0] . '</label></p>';
+
+		echo $html;
+
+	}
+
+}
+
+/**
+ * Put an instance of the class into a function.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return object Returns an instance of the class.
+ */
+function ccp_dashboard_callbacks() {
+
+	return Dashboard_Callbacks::instance();
+
+}
+
+// Run an instance of the class.
+ccp_dashboard_callbacks();
