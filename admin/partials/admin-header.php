ac151dea7edb2db50ecf18f7836cc338b8862aef
ControlledChaos <greg@ccdzine.com>
2018-10-30 13:01:17 -0800

    Applied filters to admin header variables.

--- admin/partials/admin-header.php
+++ admin/partials/admin-header.php
@@ -15,22 +15,39 @@ namespace CC_Plugin\Admin;
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 /**
- * Site descrition, if any.
+ * Admin header variables.
+ *
+ * @since 1.0.0
  */
+
+// Get the site title.
 $title       = get_bloginfo( 'name' );
+
+// Get the site tagline.
 $description = get_bloginfo( 'description' );
 
+// Return null if no site title.
 if ( ! empty( $title ) ) {
     $title = get_bloginfo( 'name' );
 } else {
-    $title = '';
+    $title = null;
 }
 
+// Return a reminder if no site tagline.
 if ( ! empty( $description ) ) {
     $description = get_bloginfo( 'description' );
 } else {
     $description = __( 'Add a tagline in Settings > General or change this in', 'controlled-chaos-plugin' ) . ' <code>controlled-chaos-plugin/admin/partials/admin-header.php</code>';
-} ?>
+}
+
+// Get the admin menu registered in `class-admin-pages.php`.
+$menu = 'admin-header';
+
+// Apply filters to the variables.
+$title       = apply_filters( 'ccp_admin_header_title', $title );
+$description = apply_filters( 'ccp_admin_header_description', $description );
+$menu        = apply_filters( 'ccp_admin_header_menu', $menu );
+?>
 <?php do_action( 'ccp_before_admin_header' ); ?>
 <header class="ccp-admin-header">
     <?php do_action( 'ccp_before_admin_site_branding' ); ?>
@@ -43,7 +60,7 @@ if ( ! empty( $description ) ) {
     <nav class="admin-navigation">
         <?php wp_nav_menu(
             array(
-                'theme_location'  => 'admin-header',
+                'theme_location'  => $menu,
                 'container'       => false,
                 'menu_id'         => 'admin-navigation-list',
                 'menu_class'      => 'admin-navigation-list',
