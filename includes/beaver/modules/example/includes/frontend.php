479f3a41c22cbc5d7321dc35b7a2d77d964211df
ControlledChaos <greg@ccdzine.com>
2018-11-15 11:00:46 -0800

    Docblocks and security checks added to some example Beaver Builder files.

--- includes/beaver/modules/example/includes/frontend.php
+++ includes/beaver/modules/example/includes/frontend.php
@@ -1,16 +1,24 @@
 <?php
-
 /**
+ * Example module frontend
+ *
  * This file should be used to render each module instance.
- * You have access to two variables in this file: 
- * 
+ * You have access to two variables in this file:
+ *
  * $module An instance of your module class.
  * $settings The module's settings.
  *
- * Example: 
+ * @package    Controlled_Chaos_Plugin
+ * @subpackage Includes\Beaver
+ *
+ * @since      1.0.0
+ * @author     Greg Sweet <greg@ccdzine.com>
  */
 
-?>
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+} ?>
 <div class="fl-example-text">
     <?php echo $settings->textarea_field; ?>
     <?php $module->example_method(); ?>
--- includes/beaver/modules/example/includes/settings-example.php
+++ includes/beaver/modules/example/includes/settings-example.php
@@ -1,15 +1,30 @@
+<?php
+/**
+ * Example module settings
+ *
+ * @package    Controlled_Chaos_Plugin
+ * @subpackage Includes\Beaver
+ *
+ * @since      1.0.0
+ * @author     Greg Sweet <greg@ccdzine.com>
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+} ?>
 <div class="fl-builder-settings-section">
-            
-    <h3 class="fl-builder-settings-title"><?php _e('Include Example', 'controlled-chaos-plugin'); ?></h3>
-            
-    <p><?php _e('This tab was created using a file instead of a sections array!', 'controlled-chaos-plugin'); ?></p>
-                    
+
+    <h3 class="fl-builder-settings-title"><?php _e( 'Include Example', 'controlled-chaos-plugin' ); ?></h3>
+
+    <p><?php _e( 'This tab was created using a file instead of a sections array!', 'controlled-chaos-plugin' ); ?></p>
+
     <table class="fl-form-table">
         <tr>
-            <th><?php _e('Text', 'controlled-chaos-plugin'); ?></th>
+            <th><?php _e( 'Text', 'controlled-chaos-plugin' ); ?></th>
             <td>
-                <input type="text" name="included_text" value="<?php if(isset($settings->included_text)) echo $settings->included_text; ?>" class="text text-full">
-            </td>                    
+                <input type="text" name="included_text" value="<?php if ( isset( $settings->included_text ) ) { echo $settings->included_text; } ?>" class="text text-full">
+            </td>
         </tr>
     </table>
 
