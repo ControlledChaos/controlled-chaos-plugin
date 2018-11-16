1eee30087b7d319697c2e00516856586f8d11f40
ControlledChaos <greg@ccdzine.com>
2018-11-16 12:06:52 -0800

    Begin support for custom Elementor widgets.

---
+++ includes/elementor/class-elementor.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Provided for custom Elementor widgets.
+ *
+ * The example widget here is taken from the official Elementor
+ * example on creating a new widget.
+ *
+ * @link       https://developers.elementor.com/creating-a-new-widget/
+ *
+ * @package    Controlled_Chaos_Plugin
+ * @subpackage Includes\Beaver
+ *
+ * @since      1.0.0
+ * @author     Greg Sweet <greg@ccdzine.com>
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Elementor class.
+ *
+ * @since  1.0.0
+ * @access public
+ */
+class Elementor_oEmbed_Widget extends \Elementor\Widget_Base {
+
+	/**
+	 * Get widget name.
+	 *
+	 * Retrieve oEmbed widget name.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return string Widget name.
+	 */
+	public function get_name() {
+		return 'oembed';
+	}
+
+	/**
+	 * Get widget title.
+	 *
+	 * Retrieve oEmbed widget title.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return string Widget title.
+	 */
+	public function get_title() {
+		return __( 'oEmbed', 'plugin-name' );
+	}
+
+	/**
+	 * Get widget icon.
+	 *
+	 * Retrieve oEmbed widget icon.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return string Widget icon.
+	 */
+	public function get_icon() {
+		return 'fa fa-code';
+	}
+
+	/**
+	 * Get widget categories.
+	 *
+	 * Retrieve the list of categories the oEmbed widget belongs to.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return array Widget categories.
+	 */
+	public function get_categories() {
+		return [ 'general' ];
+	}
+
+	/**
+	 * Register oEmbed widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 */
+	protected function _register_controls() {
+
+		$this->start_controls_section(
+			'content_section',
+			[
+				'label' => __( 'Content', 'plugin-name' ),
+				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+		$this->add_control(
+			'url',
+			[
+				'label' => __( 'URL to embed', 'plugin-name' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'input_type' => 'url',
+				'placeholder' => __( 'https://your-link.com', 'plugin-name' ),
+			]
+		);
+
+		$this->end_controls_section();
+
+	}
+
+	/**
+	 * Render oEmbed widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 */
+	protected function render() {
+
+		$settings = $this->get_settings_for_display();
+
+		$html = wp_oembed_get( $settings['url'] );
+
+		echo '<div class="oembed-elementor-widget">';
+
+		echo ( $html ) ? $html : $settings['url'];
+
+		echo '</div>';
+
+	}
+
+}
+
+// Run the class.
+$ccp_elementor = new CCP_Elementor;
